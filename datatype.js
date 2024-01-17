b93881ce742ff18ac106ede6a8fbf192635db6be
sssshahrukh <156694010+sssshahrukh@users.noreply.github.com>
2024-01-17 06:42:30 +0000

    datatype

---
+++ datatype.js
@@ -0,0 +1,14 @@
+let name="shahrukh";
+let age= 34;
+let login= false;
+
+// premitive datatype
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefine
+// symbol => unique
+
+// object
---
+++ variable.js
@@ -0,0 +1,13 @@
+const accountId = 234355;
+let accountEmail = "shahrukh@gamil.com";
+var accountPassword = "48683";
+accountCity ="delhi";
+let accountState;
+
+// accountId = 2
+accountEmail="hgwoe"
+accountPassword="444444"
+accountCity="jaipur"
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
